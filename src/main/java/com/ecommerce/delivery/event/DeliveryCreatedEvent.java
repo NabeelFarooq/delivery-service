16707b3d7118e24a1f0e866cded6f750680c624c
NabeelFarooq <nabeel007dar@gmail.com>
2026-09-24 12:08:19 +0530

    add events

---
+++ src/main/java/com/ecommerce/delivery/event/DeliveryCreatedEvent.java
@@ -0,0 +1,82 @@
+package com.ecommerce.delivery.event;
+
+import java.time.LocalDateTime;
+
+public class DeliveryCreatedEvent {
+
+    private String eventId;
+    private String eventType;
+    private String orderId;
+
+    private int customerId;
+
+    private String trackingNumber;
+    private String deliveryAddress;
+    private String deliveryStatus;
+
+    private LocalDateTime eventTime;
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public String getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(String eventType) {
+        this.eventType = eventType;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(int customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getTrackingNumber() {
+        return trackingNumber;
+    }
+
+    public void setTrackingNumber(String trackingNumber) {
+        this.trackingNumber = trackingNumber;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getDeliveryStatus() {
+        return deliveryStatus;
+    }
+
+    public void setDeliveryStatus(String deliveryStatus) {
+        this.deliveryStatus = deliveryStatus;
+    }
+
+    public LocalDateTime getEventTime() {
+        return eventTime;
+    }
+
+    public void setEventTime(LocalDateTime eventTime) {
+        this.eventTime = eventTime;
+    }
+}
